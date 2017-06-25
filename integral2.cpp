a4af199de157aea80f483c744b9cb36aa5a1fca0
Sandro <ssillva@bol.com.br>
2017-06-25 00:12:31 -0300

    Update integral2.cpp

--- integral2.cpp
+++ integral2.cpp
@@ -2,35 +2,39 @@
 
 #include <iostream>
 #include <locale>
-#include <math.h>
-#include <iomanip>
+#include <math.h> //utilizada para a funcao sin(), para calculo do seno
+#include <stdlib.h> //utilizada para a funcao getchar(), pausa a execucao do programa
+#include <stdio.h> //utilizada para a funcao system(), com o comando "clear" quando for linux ou "cls" quando for windows
 
  using namespace std;
 
-// função da integral
-  double func1(double x){
+// função da integral da primeira opcao
+double func1(double x){
 
-    return 1 / x;//3 *( x * x);
+    return 1 / x;
 
- }
- double func2(double x){
-		double valor = x * x * x;
+}
+//funcao integral da segunda opcao
+double func2(double x){
+		double valor = x * x * x; // x³ equivalente a x ao cubo
 		return sin(valor);
-	 
 }
 
+double func3(double x){
+	return 4 + 3 *(x*x); //4+3x²
+}
 // calculo do delta x
  double deltax (double aa,double bb, double nn){
 
     return (bb - aa) / nn;
 
  }
-
- double integral(int n){
+//funcao para calcular o valor da integral com a regra do ponto medio
+ double integral(int n, double aa, double bb, int opc){
 	double xi[n];
     // Intervalos
-    double a = 1.0;
-    double b = 2.0;
+    double a = aa;
+    double b = bb;
     double sfunc=0;
     double dx = deltax(a, b, n);
     for(int i=1 ; i <= n;i++){
@@ -40,26 +44,73 @@
 	}
 	
 	for(int i=1 ; i <= n;i++){
+		//escolhe de acordo com a opcao enviada
+		if (opc == 1)
 			sfunc += func1((xi[i-1]+xi[i])/2);
+		else if (opc == 2)
+			sfunc += func2((xi[i-1]+xi[i])/2);
+		else if (opc == 3)
+			sfunc += func3((xi[i-1]+xi[i])/2);
+		else
+			cout << "opcao invalida";
 	}
+	//calcula a integral multiplicando o delta e o somatorio da funcao
 	double integralx = dx * sfunc;
 	
     
 	return integralx;
 }
- 
-int main(){
-	setlocale(LC_ALL, "Portuguese");
-	int quantidadeRetangulos[] = {5,10,50,100,500,10000};
+ //funcao com uma tela do resultado do calculo da integral
+ void menu(int a, int b, int opc){
+	 //vetor para calcular os retangulos
+	 int quantidadeRetangulos[] = {5,10,50,100,500,10000};
 	double n = 0;
 	cout<<"============================================================"<<endl;
 	cout<<"|	n	|                             |  Integral  |"<<endl;
 	cout<<"============================================================"<<endl;
 	for(int i=0;i<6; i++){
 		n = quantidadeRetangulos[i];
-	cout<<"|	"<<n<<"	|                             | "<<integral(n)<<endl;
+		cout<<"|	"<<n<<"	|                             | "<<integral(n, a, b, opc)<<endl;}
+		getchar();
 }
-
+ //funcao principal
+int main(){
+	int opc;
+	setlocale(LC_ALL, "Portuguese");
+	//enquanto o usuario nao digitar 4 pra sair, a tela entra em loop..
+	while (opc != 4){
+	cout << "QUAL A FUNCAO A REALIZAR O CALCULO DA INTEGRAL?\n\n";
+	cout << "1 - f(x) = 1/x \t\t(intervalo a = 1 e b = 2)\n2 - f(x) = sen(x^3)\t(intervalo a = 1 e b = 2\n3 - f(x) = 4 + 3x^2\t(intervalo a = 0 e b = 1)";
+	cout << "\n4 - SAIR";
+	cout << "\nDigite a Opcao: ";
+	cin >> opc;
+	//executa as opcoes de acordo com a escolha do usuario
+	switch (opc){
+			case 1:
+				menu(1, 2, 1);
+				getchar();
+				//system("cls");
+				system("clear");
+				break;
+			case 2:
+				menu(1, 2, 2);
+				getchar();
+				//system("cls");
+				system("clear");
+				break;
+			case 3:
+				menu(0, 1, 3);
+				getchar();
+				//system("cls");
+				system("clear");
+				break;
+			case 4:
+				break;
+			default:
+				system("clear");
+				cout << "Opcao Invalida\n";
+				break;
+		}
+	}
     return 0;
-
- }
+}
