e84b9e6a9adc5eda640e32b7f008fce3a3250cc6
Sandro <sandro2007am@gmail.com>
2022-11-07 12:01:31 -0300

    Update integral2.cpp
    Cálculo 2, atualizando funções

--- integral2.cpp
+++ integral2.cpp
@@ -2,29 +2,30 @@
 
 #include <iostream>
 #include <locale>
-#include <math.h> //utilizada para a funcao sin(), para calculo do seno
+#include <math.h> //utilizada para a funcao matemáticas
 #include <cstdlib> //utilizada para a funcao system(), com o comando "clear" quando for linux ou "cls" quando for windows
-#include <stdio.h>//utilizada para a funcao getchar(), pausa a execucao do programa 
+#include <stdio.h>//utilizada para a funcao getchar(), pausa a execucao do programa
 
  using namespace std;
 
 // função da integral da primeira opcao
 double func1(double x){
 
-    return (pow(x,3) / 3);
+    return (pow(x-1,10));
 
 }
 //funcao integral da segunda opcao
 double func2(double x){
-		return 4 - (pow(x,2)/4);
+		return (sqrt(x+1));
 }
 
 double func3(double x){
-	
-	return 1 / x;
+	double y;
+	y = pow(x, 2) + 1;
+	return ((2*x) * pow(y,3));
 }
 double func4(double x){
-		return(x*x);
+		return (((2*x)+1)* (pow(x, 2)+x));
 }
 // calculo do delta x
  double deltax (double aa,double bb, double nn){
@@ -45,7 +46,7 @@ double func4(double x){
 			xi[i] = xi[i-1] + dx;
 			//cout << xi[i];
 	}
-	
+
 	for(int i=1 ; i <= n;i++){
 		//escolhe de acordo com a opcao enviada
 		if (opc == 1)
@@ -61,15 +62,15 @@ double func4(double x){
 	}
 	//calcula a integral multiplicando o delta e o somatorio da funcao
 	double integralx = dx * sfunc;
-	
-    
+
+
 	return integralx;
 }
  //funcao com uma tela do resultado do calculo da integral
  void menu(int a, int b, int opc){
 	 //vetor para calcular os retangulos
 	 int quantidadeRetangulos[] = {5,10,50,100,500,10000};
-	
+
 	double n = 0;
 	cout<<"============================================================"<<endl;
 	cout<<"|	n	|                             |  Integral  |"<<endl;
@@ -84,36 +85,40 @@ int main(){
 	char limpar[] = "clear";
 	int opc;
 	setlocale(LC_ALL, "Portuguese");
-	//enquanto o usuario nao digitar 4 pra sair, a tela entra em loop..
+	//enquanto o usuario nao digitar 5 pra sair, a tela entra em loop..
 	while (opc != 5){
 	cout << "QUAL A FUNCAO A REALIZAR O CALCULO DA INTEGRAL?\n\n";
-	cout << "1 - f(x) = (x^3 / 3) / 3 \t\t(intervalo a = -1 e b = 2)\n2 - f(x) = 4 - (x^2) / 4 \t\t(intervalo a = -4 e b = 4\n3 - f(x) = 1 / x\t\t\t(intervalo a = 1 e b = 3)";
-	cout << "\n4 - f(x) = x^2\t\t\t\t(intervalo a = 1 e b = 3)";
+	cout << "1 - f(x) = (x - 1)^10 \t\t\t(intervalo a = 0 e b = 1)\n2 - f(x) = sqrt(x+1) \t\t\t(intervalo a = 1 e b = 3)\n3 - f(x) = 2x * (x^2 + 1)^3\t\t(intervalo a = 1 e b = 5)";
+	cout << "\n4 - f(x) = (2x+1) * (x^2 + x)\t\t(intervalo a = 1 e b = 5)";
 	cout << "\n5 - SAIR";
 	cout << "\nDigite a Opcao: ";
 	cin >> opc;
 	//executa as opcoes de acordo com a escolha do usuario
 	switch (opc){
 			case 1:
-				menu(-1, 2, 1);
+				//intervalo (0 a 1)
+				menu(0, 1, 1);
 				getchar();
 				//system("cls");
 				system(limpar);
 				break;
 			case 2:
-				menu(-4, 4, 2);
+				//intervalo (1 a 3)
+				menu(1, 3, 2);
 				getchar();
 				//system("cls");
 				system(limpar);
 				break;
 			case 3:
-				menu(1, 3, 3);
+				//intervalo (1 a 5)
+				menu(1, 5, 3);
 				getchar();
 				//system("cls");
 				system(limpar);
 				break;
 			case 4:
-				menu(1, 3, 4);
+				//intervalo (1 a 5)
+				menu(1, 5, 4);
 				getchar();
 				//system("cls");
 				system(limpar);
